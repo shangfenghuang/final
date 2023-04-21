f0c5682004333f84c17d8e5cce3464e0b52ca5df
feng <45654745+shangfenghuang@users.noreply.github.com>
2023-04-20 22:26:28 -0600

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -188,7 +188,7 @@ function load_map(style) {
     console.log('start');
     intervalId = setInterval(function() {
       getLocation();
-    }, 30000);
+    }, 10000);
   });
 
   exitNavigationBtn.addEventListener('click', function (){
